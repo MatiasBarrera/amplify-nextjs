6af0094f21d7bc3dbdb221efcfa8479fb4ba9e6d
MatiasBarrera <matias.barrera95@gmail.com>
2022-02-14 22:12:28 -0300

    fix: next.config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ module.exports = {
   images: {
     domains: ['cs-stores-images.s3.amazonaws.com', 'imgur.com', 'comeschile.cl'],
   },
+  env: {
+    NEXT_PUBLIC_IMAGES_PATH: process.env.NEXT_PUBLIC_IMAGES_PATH,
+    API_URL: process.env.API_URL,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_WHATSAPP_NUMBER: process.env.NEXT_PUBLIC_WHATSAPP_NUMBER,
+  },
   webpack: (config, { defaultLoaders, isServer }) => {
     if (isServer) {
       config.externals.push('_http_common')
